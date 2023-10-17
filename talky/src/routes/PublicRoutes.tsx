04393975cee77134674fdec9acbfd5c29cb7e359
Liel Almog <70017134+liel-almog@users.noreply.github.com>
2023-10-17 03:52:53 +0000

    Fix route problems

--- talky/src/routes/PublicRoutes.tsx
+++ talky/src/routes/PublicRoutes.tsx
@@ -8,6 +8,7 @@ import { Layout } from "../pages/Layout";
 import { Rooms } from "../pages/Rooms";
 import { Welcome } from "../pages/Welcome";
 import { BaseErrorBoundary } from "../components/common/BaseErrorBoundary";
+import { Chat } from "../pages/Chat";
 
 export interface PublicRoutesProps {}
 
@@ -15,7 +16,10 @@ export const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="*" element={<Layout />}>
       <Route index element={<Welcome />} />
-      <Route path="room" element={<Rooms />}></Route>
+      <Route path="room">
+        <Route index element={<Rooms />} />
+        <Route path=":roomId/chat" element={<Chat />} />
+      </Route>
     </Route>
   )
 );
